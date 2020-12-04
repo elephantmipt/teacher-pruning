4e013f77cdbcb8de3fa0604a88a88ca2f2520d80
Nikita <Balaganskij.NN@phystech.edu>
2020-12-04 19:43:25 +0300

    fix resnet

---
+++ src/models/resnet.py
@@ -0,0 +1,21 @@
+from modules.resnet import PreActResNet, PreActBlock, PreActBottleneck
+
+
+def PreActResNet18():
+    return PreActResNet(PreActBlock, [2, 2, 2, 2])
+
+
+def PreActResNet34():
+    return PreActResNet(PreActBlock, [3, 4, 6, 3])
+
+
+def PreActResNet50():
+    return PreActResNet(PreActBottleneck, [3, 4, 6, 3])
+
+
+def PreActResNet101():
+    return PreActResNet(PreActBottleneck, [3, 4, 23, 3])
+
+
+def PreActResNet152():
+    return PreActResNet(PreActBottleneck, [3, 8, 36, 3])
--- src/train.py
+++ src/train.py
@@ -8,8 +8,9 @@ from torch.utils.data import DataLoader
 
 import wandb
 
-from torchvision import transforms as T
-from torchvision import datasets, models
+from torchvision import transforms
+from torchvision import datasets
+from models.resnet import PreActResNet18
 from catalyst import dl
 from catalyst.utils import set_global_seed
 
@@ -19,16 +20,18 @@ from callbacks.wandb import WandbCallback
 def main(args):
     wandb.init(project="teacher-pruning", config=vars(args))
     set_global_seed(42)
-    transform_train = T.Compose([
-        T.RandomCrop(32, padding=4),
-        T.RandomHorizontalFlip(),
-        T.ToTensor(),
-        T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
     ])
 
-    transform_test = T.Compose([
-        T.ToTensor(),
-        T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
     ])
 
     train_dataset = datasets.CIFAR10(
@@ -38,25 +41,24 @@ def main(args):
         root=os.getcwd(), train=False, transform=transform_test
     )
     train_dataloader = DataLoader(
-        dataset=train_dataset, batch_size=args.batch_size, shuffle=True
+        dataset=train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2
     )
-    valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=128)
+    valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=128, num_workers=2)
     loaders = {
         "train": train_dataloader,
         "valid": valid_dataloader,
     }
 
-    model = models.resnet18(pretrained=False)
+    model = PreActResNet18()
     model.fc = nn.Linear(512, 10)
 
-    optimizer = torch.optim.Adam(
+    optimizer = torch.optim.SGD(
         model.parameters(),
         lr=args.lr,
         momentum=args.momentum,
-        weight_decay=0.0005
     )
     scheduler = MultiStepLR(
-        optimizer, milestones=[66, 122], gamma=args.gamma
+        optimizer, milestones=[80, 120], gamma=args.gamma
     )
     runner = dl.SupervisedRunner(device=args.device)
     logdir=f"logs/{wandb.run.name}"
@@ -71,7 +73,7 @@ def main(args):
             dl.AccuracyCallback(num_classes=10),
             WandbCallback()
         ],
-        num_epochs=200,
+        num_epochs=args.epoch,
         logdir=logdir,
         verbose=True
     )
@@ -83,8 +85,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch-size", default=128, type=int)
     parser.add_argument("--momentum", default=0.9, type=float)
     parser.add_argument("-d", "--device", default="cuda:0", type=str)
-    parser.add_argument("--T-max", default=200, type=int)
-    parser.add_argument("--epoch", default=150, type=int)
+    parser.add_argument("--epoch", default=200, type=int)
     parser.add_argument("--gamma", default=0.1, type=float)
     args = parser.parse_args()
     main(args)
